6154585cd3be422377d03ae55b480eaff090b14f
Rits1272 <ritikjain1272@gmail.com>
2019-10-08 02:29:42 +0530

    Initial Commits

---
+++ components/env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "notessharing",
+    "private_key_id": "3b1ad2360163fc1049be6ed4e4f8728e5d77e032",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDe7Zj/Ptkq2Hb0\nRd+W+PD/yIJEB7cXnETX8uiqgGrH6RqET4cPct9c+umALhqQVYTUf06XxJePXExj\nSwGF2exVT490odXiaDf5PIgj0cj8LSZZXoUlCoQesmCoA50zGnwQRJZ98fC2QPzO\nQeTEoleRmjMk52RUpJPA5H+7vjMxkyRo69xSM3jP/851zDld25QnVJ493ItAXaKf\nDDwOeF1tLpGtyUkksbmHvp8MlUfkxv3GMefn8X0GXuJqFeMK59cv56ptzBC8abUa\nyB/oKc1Lm/HJpMLkF0h80UwEwrZeaVHaDW6x7xl5m+wnhtDuQjLnpueRTTkNcQ8M\ny1ZuztoXAgMBAAECggEABp9yayrX0tEJf6pPwTF2H3M6g4DjLJ1k4jGCPgaApZ3E\nPugJ8CiOgDmoFDnKaj1QK+v4TsP1aLnqPwzsZ6iYv9QUQ/fG3eoQt6PXu5Cw8uT4\nfbE0LWyJjkvoWTV6RxljVCxleCpm51M473u/qyPTjCzTliYVsfoNta5UKSS2aqOR\nQDS04S3zW5LjMI75BJ37NLVVCF3EYnEJq3tgy70u8lFs3sfZiCmq7MKhTrvPAHnn\nd37nHHDR+83HiyhEiwTUsW3CTMUzsyY1m5fM1rTWDOaTsUY54pyjalJnzUbfVnZw\ne3zgP/soMQQRyrK5qGYZsyEXOwAj94Im+xFWR7gOUQKBgQDySrQ0uI/3bycf7hvI\nh3slN6Gp5+0Khfv6aUAattPOkIxj7wtnVo80WxeQdvP28KoI0gmqaM3eHuGZVgum\nYVAF27WaipvUAduGlaRJ4TLGkT5jpVcMwrvIjIXhQuNgeTqpdve23bX4wkUbaEPk\nRAHZBITXXd7eC1M7LtrufJKnnwKBgQDrim9C5ewDnD6+pNt96IWJAtMzwDODVI6c\nLT6d17YWOYtGkYxFd/yCfQxKfleWmlJY1EoVWUfax3A5C6ZZKCnNnPp62JqL1gws\n999GpIajn5d2OzFKM/WYiA0Twab6u4fxXtSJTWXw9vF3Xkob+6wGGf1U9IhkyMYQ\nMHgvAZMaiQKBgQCKYxC6bBAOi3YVTrKfvoIfp762hLlnIU/inur2VyZy/Syyh35D\n77Srerviz45YpOz5TL4ojuPG0myYYAUXas2CCxOWyURnuwffvuivKekOcTbpQy6N\nI0yOJ3TwTHwCRjuzPzgNQmUFSDCgjtT4M7+6bFJTkFITEPThAMobHRvGkQKBgQDL\npNbpSgyyWmZNQg+bXmwFt3GPLr/a7kKWeO/IXHZL5Nir3K8gaMrirubviSiFR0un\nDXQ33Qco+oHmcFIUh4sNAIn3qEqGWDwSrQRX+WuHEDZNvIOzGhVfGVHlIOsToQu5\n/jlXtJlMqtS+VcFRTFjAtbu3tewu5v+Hg8CFM6FRsQKBgQDnJHJZq2M5WHllCHRX\n68/hbt9Vyv5MdXiNOQGRVTqKdHsDOEuHcnpw8PEsnrGOVvO8/WJICWctQvFQh6WR\nWBC1l2BDX9sOhm95A/jgxhB5jIqAZVeJ36fpiAcJYolz688Sm4309TXJAm/TSRS2\nBTtdtkz4/Q+Uy46fxeyPMm0ZqA==\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-otgsbv@notessharing.iam.gserviceaccount.com",
+    "client_id": "104436469342335622459",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-otgsbv%40notessharing.iam.gserviceaccount.com"
+  }
+  
